0cf1addcacf36d4fbf1e598bd08398d0d0796188
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2023-10-25 11:58:51 +0700

    Create Employee.cs
    Xây dựng Model Employee

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using bai1.Models;
+
+namespace bai1.Models
+{
+    
+    public class Employee
+    {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
